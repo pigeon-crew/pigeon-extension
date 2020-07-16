8f8ce06e689676731bff93df56e1ee003869a8dd
Daniel Tian <dtian.95@gmail.com>
2020-07-17 00:15:27 +0800

    created dummy pigeon feed

---
+++ src/popup/components/PFItem.js
@@ -0,0 +1,40 @@
+/** @format */
+
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+
+const FeedContainer = styled.div`
+  width: 240px;
+  border-radius: 12px;
+  background-color: #f5f6f8;
+  margin: auto;
+  padding: 2px 10px;
+  margin-bottom: 20px;
+`;
+
+const URLHeader = styled.p`
+  font-size: 14px;
+  font-family: 'Avenir';
+  font-weight: 600;
+  color: #797979;
+`;
+
+const Footer = styled.p`
+  font-size: 12px;
+  font-family: 'Avenir';
+  font-weight: 400;
+  color: #797979;
+`;
+
+const PFItem = ({ data }) => {
+  return (
+    <FeedContainer>
+      <URLHeader>{data.link}</URLHeader>
+      <Footer>
+        {data.sender} | {data.timestamp}
+      </Footer>
+    </FeedContainer>
+  );
+};
+
+export default PFItem;
--- src/popup/pages/Popup.js
+++ src/popup/pages/Popup.js
@@ -1,27 +1,34 @@
-import React from "react";
-import styled from "styled-components";
+/** @format */
+
+import React from 'react';
+import styled from 'styled-components';
+import PFItem from '../components/PFItem';
 
 const PopupContainer = styled.div`
-  text-align: center;
+  text-align: left;
   width: 310px;
-  min-height: 290px;
+  max-height: 290px;
+`;
+
+const ContentContainer = styled.div`
+  margin: 20px;
 `;
 
 const Headline = styled.p`
   font-size: 20px;
-  font-family: "Avenir";
+  font-family: 'Avenir';
   font-weight: 600;
   color: white;
 `;
 
 // TODO: Eventually use Formik to hook up?
 const URLInputField = styled.input`
-  width: 200px;
+  width: 230px;
   border-radius: 12px;
   color: #797979;
   background-color: #f5f6f8;
   padding: 8px 16px;
-  font-family: "Avenir";
+  font-family: 'Avenir';
   font-weight: 400;
   font-size: 14px;
   border: 3px solid #f5f6f8;
@@ -38,23 +45,45 @@ const URLInputField = styled.input`
   }
 `;
 
-const FeedContainer = styled.div`
-  width: 200px;
-  border-radius: 12px;
-  background-color: #f5f6f8;
-  height: 40px;
-  margin: auto;
-  margin-bottom: 20px;
-`;
+const fakeData = [
+  {
+    sender: 'Jane Doe',
+    link: 'The Verge was founded in 2011 in partnership with Vox Media',
+    timestamp: '5hrs ago',
+  },
+  {
+    sender: 'John Smith',
+    link: 'The Verge was founded in 2011 in partnership with Vox Media',
+    timestamp: '5hrs ago',
+  },
+  {
+    sender: 'John Smith',
+    link: 'The Verge was founded in 2011 in partnership with Vox Media',
+    timestamp: '5hrs ago',
+  },
+  {
+    sender: 'John Smith',
+    link: 'The Verge was founded in 2011 in partnership with Vox Media',
+    timestamp: '5hrs ago',
+  },
+  {
+    sender: 'John Smith',
+    link: 'The Verge was founded in 2011 in partnership with Vox Media',
+    timestamp: '5hrs ago',
+  },
+];
 
 const Popup = () => {
   return (
     <PopupContainer>
-      <Headline>Share this link with</Headline>
-      <URLInputField name="url" placeholder="Hit Enter to Send" />
-      <Headline>Pigeon Feed</Headline>
-      <FeedContainer />
-      <FeedContainer />
+      <ContentContainer>
+        <Headline>Share this link with</Headline>
+        <URLInputField name="url" placeholder="Hit Enter to Send" />
+        <Headline>Pigeon Feed</Headline>
+        {fakeData.map((val) => {
+          return <PFItem data={val} />;
+        })}
+      </ContentContainer>
     </PopupContainer>
   );
 };
