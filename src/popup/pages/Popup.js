a417e2ce6fcae9a5a8d7f53bd18ee750a981dab6
LeytonHo <leyton.ho@gmail.com>
2020-07-15 23:33:17 -0700

    Very preliminary popup styling

--- src/popup/pages/Popup.js
+++ src/popup/pages/Popup.js
@@ -3,15 +3,58 @@ import styled from "styled-components";
 
 const PopupContainer = styled.div`
   text-align: center;
-  width: 410px;
+  width: 310px;
   min-height: 290px;
+`;
+
+const Headline = styled.p`
+  font-size: 20px;
+  font-family: "Avenir";
+  font-weight: 600;
+  color: white;
+`;
+
+// TODO: Eventually use Formik to hook up?
+const URLInputField = styled.input`
+  width: 200px;
+  border-radius: 12px;
+  color: #797979;
+  background-color: #f5f6f8;
+  padding: 8px 16px;
+  font-family: "Avenir";
+  font-weight: 400;
+  font-size: 14px;
+  border: 3px solid #f5f6f8;
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.4);
+  }
+
+  &:focus {
+    outline: none;
+    background: white;
+    border: 3px solid #ff8686;
+    color: black;
+  }
+`;
+
+const FeedContainer = styled.div`
+  width: 200px;
+  border-radius: 12px;
+  background-color: #f5f6f8;
+  height: 40px;
+  margin: auto;
+  margin-bottom: 20px;
 `;
 
 const Popup = () => {
   return (
     <PopupContainer>
-      <h1>Hey! This is Pigeon</h1>
-      <p>Our mission is to make link sharing easy and fun for you.</p>
+      <Headline>Share this link with</Headline>
+      <URLInputField name="url" placeholder="Hit Enter to Send" />
+      <Headline>Pigeon Feed</Headline>
+      <FeedContainer />
+      <FeedContainer />
     </PopupContainer>
   );
 };
