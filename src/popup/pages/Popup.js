030b8098f1a1e0e02765b1ce4af277aa6a945636
LeytonHo <leyton.ho@gmail.com>
2020-07-15 22:32:49 -0700

    Refactor Popup.js using styled-components

--- src/popup/pages/Popup.js
+++ src/popup/pages/Popup.js
@@ -1,12 +1,18 @@
 import React from "react";
-import "../styles/Popup.css";
+import styled from "styled-components";
+
+const PopupContainer = styled.div`
+  text-align: center;
+  width: 410px;
+  min-height: 290px;
+`;
 
 const Popup = () => {
   return (
-    <div className="popup">
+    <PopupContainer>
       <h1>Hey! This is Pigeon</h1>
       <p>Our mission is to make link sharing easy and fun for you.</p>
-    </div>
+    </PopupContainer>
   );
 };
 
