4c9e1fc314585a43a76dcfb35f075c8efc4473f3
LeytonHo <leyton.ho@gmail.com>
2020-08-04 12:03:42 -0700

    Add styling for manage contacts

--- src/popup/components/FriendReqItem.js
+++ src/popup/components/FriendReqItem.js
@@ -1,6 +1,39 @@
 /** @format */
 
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+const Subtitle = styled.h3`
+  font-size: 16px;
+  font-family: "Avenir";
+  font-weight: 600;
+  color: rgb(72, 72, 72, 0.8);
+  margin: 10px 0 0 0;
+  padding: 0px;
+`;
+
+const StyledButton = styled.button`
+  border: none;
+  background-color: #f5f5f5;
+  padding: 5px 15px;
+  border-radius: 10px;
+  font-size: 16px;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Avenir";
+  font-size: 12px;
+  font-weight: 600px;
+  margin: 5px 10px 5px 0px;
+
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 3px 10px;
+    opacity: 0.9;
+    background-color: rgba(231, 84, 128, 1);
+  }
+  &:active {
+    opacity: 0.6;
+  }
+`;
 
 const FriendReqItem = ({ data, handleAccept, handleReject }) => {
   useEffect(() => {
@@ -13,21 +46,21 @@ const FriendReqItem = ({ data, handleAccept, handleReject }) => {
 
   return (
     <div key={data._id}>
-      <h3>{data.requesterName}</h3>
-      <button
+      <Subtitle>{data.requesterName}</Subtitle>
+      <StyledButton
         onClick={() => {
           handleAccept(data._id);
         }}
       >
         Accept
-      </button>
-      <button
+      </StyledButton>
+      <StyledButton
         onClick={() => {
           handleReject(data._id);
         }}
       >
         Reject
-      </button>
+      </StyledButton>
     </div>
   );
 };
--- src/popup/pages/Contact.js
+++ src/popup/pages/Contact.js
@@ -1,8 +1,90 @@
 /** @format */
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { goBack } from 'react-chrome-extension-router';
-import FriendReqItem from '../components/FriendReqItem';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { goBack } from "react-chrome-extension-router";
+import FriendReqItem from "../components/FriendReqItem";
+
+// TODO: Consolidate Headline, Subtitle, Desc into one component with font size as a prop
+
+const Headline = styled.h1`
+  font-size: 20px;
+  font-family: "Avenir";
+  font-weight: 600;
+  color: white;
+  margin: 10px 0;
+  padding: 0px;
+  /*text-align: center;*/
+`;
+
+const Subtitle = styled.h3`
+  font-size: 16px;
+  font-family: "Avenir";
+  font-weight: 600;
+  color: #f5f5f5;
+  margin: 20px 0 0 0;
+  padding: 0px;
+`;
+
+const Desc = styled.div`
+  font-size: 14px;
+  font-family: "Avenir";
+  font-weight: 500;
+  color: white;
+  margin: 10px 0px 15px 0px;
+`;
+
+const StyledButton = styled.button`
+  border: none;
+  background-color: #f5f5f5;
+  padding: 5px 15px;
+  border-radius: 10px;
+  font-size: 16px;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Avenir";
+  font-size: 12px;
+  font-weight: 600px;
+
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 3px 10px;
+    opacity: 0.9;
+    background-color: rgba(231, 84, 128, 1);
+  }
+  &:active {
+    opacity: 0.6;
+  }
+`;
+
+const StyledInput = styled.input`
+  width: 225px;
+  height: 30px;
+  padding: 5px 20px;
+  font-family: Helvetica, sans-serif;
+  font-weight: 300;
+  font-size: 16px;
+  border: 1px solid #aaa;
+  border-radius: 8px;
+  margin: 10px 0px 10px 0px;
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  display: inline-block;
+  text-align: start;
+  appearance: textfield;
+  background-color: -internal-light-dark(rgb(255, 255, 255), rgb(59, 59, 59));
+  -webkit-rtl-ordering: logical;
+  cursor: text;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    outline: none;
+  }
+`;
 
 const ContactContainer = styled.div`
   text-align: left;
@@ -18,15 +100,15 @@ const validateEmail = (email) => {
 
 const Contact = () => {
   const [pendingRequests, setPendingRequests] = useState([]);
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   const fetchPendingRequests = () => {
-    chrome.runtime.sendMessage({ type: 'fetchPendingFriends' }, (response) => {
+    chrome.runtime.sendMessage({ type: "fetchPendingFriends" }, (response) => {
       if (response && response.success) {
         setPendingRequests(response.request);
         return;
       }
-      alert('Fetch Pending Request Error');
+      alert("Fetch Pending Request Error");
     });
   };
 
@@ -34,13 +116,13 @@ const Contact = () => {
     const payload = { friendReqId };
 
     chrome.runtime.sendMessage(
-      { type: 'acceptRequest', payload },
+      { type: "acceptRequest", payload },
       (response) => {
         if (response && response.success) {
           fetchPendingRequests();
           return;
         }
-        alert('Accept Request Error');
+        alert("Accept Request Error");
       }
     );
   };
@@ -49,13 +131,13 @@ const Contact = () => {
     const payload = { friendReqId };
 
     chrome.runtime.sendMessage(
-      { type: 'rejectRequest', payload },
+      { type: "rejectRequest", payload },
       (response) => {
         if (response && response.success) {
           fetchPendingRequests();
           return;
         }
-        alert('Reject Request Error');
+        alert("Reject Request Error");
       }
     );
   };
@@ -65,13 +147,13 @@ const Contact = () => {
 
     const payload = { email };
     chrome.runtime.sendMessage(
-      { type: 'sendFriendRequest', payload },
+      { type: "sendFriendRequest", payload },
       (response) => {
         if (response && response.success) {
-          alert('Friend request sent!');
+          alert("Friend request sent!");
           return;
         }
-        alert('Friend Request Error');
+        alert("Friend Request Error");
       }
     );
   };
@@ -86,16 +168,16 @@ const Contact = () => {
 
   return (
     <ContactContainer>
-      <h1>Manage Contact</h1>
-      <button onClick={() => goBack()}>Back</button>
-      <h3>Add Friends</h3>
-      <input
+      <Headline>Manage Contacts</Headline>
+      <StyledButton onClick={() => goBack()}>Back</StyledButton>
+      <Subtitle>Add Friends</Subtitle>
+      <StyledInput
         placeholder="Email"
         value={email}
         onChange={handleEmailChanged}
-      ></input>
-      <button onClick={handleSendRequest}>Send Request</button>
-      <h3>Pending Requests</h3>
+      ></StyledInput>
+      <StyledButton onClick={handleSendRequest}>Send Request</StyledButton>
+      <Subtitle>Pending Requests</Subtitle>
       {pendingRequests && pendingRequests.length > 0 ? (
         pendingRequests.map((data) => {
           return (
@@ -108,7 +190,7 @@ const Contact = () => {
           );
         })
       ) : (
-        <div>Your have no pending requests</div>
+        <Desc>You have no pending requests</Desc>
       )}
     </ContactContainer>
   );
